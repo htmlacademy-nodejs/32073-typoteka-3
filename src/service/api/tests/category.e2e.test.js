3e1a4fe5cf83bdf24e5a103ee91d46b156ef1c44
Kirill Madorin <madorinkv@gmail.com>
2022-04-08 20:15:02 +0300

    search and categories e2e test added, api-server moved to a standalone dir

---
+++ src/service/api/tests/category.e2e.test.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let request = require(`supertest`);
+
+const {HttpCode, API_PREFIX} = require(`../../../constants`);
+const {getServer} = require(`../../api-server`);
+const getMockData = require(`../../lib/get-mock-data`);
+
+let server;
+let mockData;
+
+beforeAll(async () => {
+  mockData = await getMockData();
+  server = await getServer(mockData);
+});
+
+describe(`Categories API end-to-end tests`, () => {
+  test(`When get categories status code should be 200`, async () => {
+    const res = await request(server).get(`${API_PREFIX}/categories`);
+    expect(res.statusCode).toBe(HttpCode.OK);
+  });
+  test(`When get categories array should be returned`, async () => {
+    const res = await request(server).get(`${API_PREFIX}/categories`);
+    expect(Array.isArray(res.body)).toBeTruthy();
+  });
+});
