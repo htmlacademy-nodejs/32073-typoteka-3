0ef7e13cb381d3f704aa556983a56aab86a70381
Kirill Madorin <madorinkv@gmail.com>
2022-04-10 21:31:29 +0300

    an update after checking by mentor

--- src/service/api/tests/category.e2e.test.js
+++ src/service/api/tests/category.e2e.test.js
@@ -6,21 +6,19 @@ const {HttpCode, API_PREFIX} = require(`../../../constants`);
 const {getServer} = require(`../../api-server`);
 const getMockData = require(`../../lib/get-mock-data`);
 
-let server;
-let mockData;
-
-beforeAll(async () => {
-  mockData = await getMockData();
-  server = getServer(mockData);
-});
-
 describe(`Categories API end-to-end tests`, () => {
+  let server;
+  let mockData;
+
+  beforeAll(async () => {
+    mockData = await getMockData();
+    server = getServer(mockData);
+  });
+
   test(`When get categories status code should be 200`, async () => {
     const res = await request(server).get(`${API_PREFIX}/categories`);
     expect(res.statusCode).toBe(HttpCode.OK);
-  });
-  test(`When get categories array should be returned`, async () => {
-    const res = await request(server).get(`${API_PREFIX}/categories`);
     expect(Array.isArray(res.body)).toBeTruthy();
   });
+
 });
