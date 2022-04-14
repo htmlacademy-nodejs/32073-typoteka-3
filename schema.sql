0d292f1581b1e250beb51c900a5fbea7f70e88c1
Kirill Madorin <madorinkv@gmail.com>
2022-04-14 19:44:14 +0300

    schema.sql added, module5-task1 completed

---
+++ schema.sql
@@ -0,0 +1,61 @@
+-- CREATE DATABASE typoteka
+--   WITH
+--   OWNER = postgres
+--   ENCODING = 'UTF8'
+--   TEMPLATE = template0
+--   LC_COLLATE = 'C'
+--   LC_CTYPE = 'C'
+--   CONNECTION LIMIT = -1;
+
+-- GRANT ALL ON DATABASE typoteka TO kmadorin;
+
+DROP TABLE IF EXISTS categories cascade;
+DROP TABLE IF EXISTS users cascade;
+DROP TABLE IF EXISTS articles cascade;
+DROP TABLE IF EXISTS comments cascade;
+DROP TABLE IF EXISTS articles_categories cascade;
+
+CREATE TABLE categories(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name varchar(255) NOT NULL
+);
+
+CREATE TABLE users(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  email varchar(255) UNIQUE NOT NULL,
+  password_hash varchar(255) NOT NULL,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  avatar varchar(50) NOT NULL
+);
+
+CREATE TABLE articles(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title varchar(255) NOT NULL,
+  announce text NOT NULL,
+  full_text text NOT NULL,
+  picture varchar(50),
+  user_id integer NOT NULL,
+  created_at timestamp DEFAULT current_timestamp,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE comments(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  article_id integer NOT NULL,
+  user_id integer NOT NULL,
+  text text NOT NULL,
+  created_at timestamp DEFAULT current_timestamp,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (article_id) REFERENCES articles(id)
+);
+
+CREATE TABLE articles_categories(
+  article_id integer NOT NULL,
+  category_id integer NOT NULL,
+  PRIMARY KEY (article_id, category_id),
+  FOREIGN KEY (article_id) REFERENCES articles(id),
+  FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE INDEX ON articles(title);
